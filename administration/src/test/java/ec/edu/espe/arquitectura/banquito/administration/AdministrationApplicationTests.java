6b3733cafa780bb6cd61659aa9cc138daab6641c
bobbyrabia <rpfreire1@espe.edu.ec>
2023-07-11 12:47:34 -0500

    initial configuration start init

---
+++ administration/src/test/java/ec/edu/espe/arquitectura/banquito/administration/AdministrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura.banquito.administration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
