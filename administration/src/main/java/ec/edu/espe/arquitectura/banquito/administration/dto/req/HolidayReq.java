2c854db80acac2f518d1dd41084c4373f00857ae
Roberto Freire <79052020+bobbyrabia@users.noreply.github.com>
2023-07-23 20:56:44 -0500

    BranchReq test

--- administration/src/main/java/ec/edu/espe/arquitectura/banquito/administration/dto/req/HolidayReq.java
+++ administration/src/main/java/ec/edu/espe/arquitectura/banquito/administration/dto/req/HolidayReq.java
@@ -3,11 +3,14 @@ package ec.edu.espe.arquitectura.banquito.administration.dto.req;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.Date;
 
 @Data
 @Builder
-public class HolidayReq {
+public class HolidayReq implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private Date holidayDate;
     private String countryCode;
     private String geoLocationId;
