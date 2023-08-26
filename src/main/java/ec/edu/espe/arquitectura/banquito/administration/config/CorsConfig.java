d662e0838479fad94e3c4047b86953be73de25aa
Roberto Freire <79052020+bobbyrabia@users.noreply.github.com>
2023-08-26 18:32:54 -0500

    .

--- src/main/java/ec/edu/espe/arquitectura/banquito/administration/config/CorsConfig.java
+++ src/main/java/ec/edu/espe/arquitectura/banquito/administration/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200","https://banquito-accounts-try-production.up.railway.app/doc/swagger-ui.html")
+                        .allowedOrigins("http://localhost:4200","https://banquito-accounts-try-production.up.railway.app/doc/swagger-ui.html","http://0.0.0.0")
                         .exposedHeaders("Content-Disposition")
                         .allowedMethods("*");
             }
